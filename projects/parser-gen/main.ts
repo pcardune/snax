f56b3048e8ca5fd21f6d5314e8b39892e4a2495c
Paul Carduner <paul@carduner.net>
2021-09-02 11:10:23 -0700

    Fix typescript errors after upgrading yargs

--- projects/parser-gen/main.ts
+++ projects/parser-gen/main.ts
@@ -2,10 +2,12 @@ import yargs from 'yargs/yargs';
 import { hideBin } from 'yargs/helpers';
 import { compileFile } from './cli';
 
-const args = yargs(hideBin(process.argv)).option('grammar', {
-  type: 'string',
-  description: 'path to the grammar file that should be compiled',
-  demandOption: true,
-}).argv;
+(async () => {
+  const args = await yargs(hideBin(process.argv)).option('grammar', {
+    type: 'string',
+    description: 'path to the grammar file that should be compiled',
+    demandOption: true,
+  }).argv;
 
-compileFile(args.grammar);
+  compileFile(args.grammar);
+})();

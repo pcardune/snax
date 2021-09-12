4a7af5446d5738bacf51aec6e631addd8a3f7771
Paul Carduner <paul@carduner.net>
2021-09-12 09:44:36 -0700

    more info with snax-cli

--- snax/projects/snax/snax-cli.ts
+++ snax/projects/snax/snax-cli.ts
@@ -1,6 +1,7 @@
 import yargs from 'yargs';
 import { hideBin } from 'yargs/helpers';
 import fs from 'fs';
+import { readFile } from 'fs/promises';
 import { compileStr } from './wat-compiler.js';
 import loadWabt from 'wabt';
 import { err, ok } from 'neverthrow';
@@ -42,21 +43,32 @@ function fileWithExtension(file: string, ext: string) {
   });
 }
 
+function builder<T>(yargs: yargs.Argv<T>) {
+  return yargs
+    .positional('file', {
+      alias: 'file',
+      describe: 'file to compile',
+      type: 'string',
+      demandOption: true,
+    })
+    .option('verbose', {
+      alias: 'v',
+      type: 'boolean',
+      description: 'Run with verbose logging',
+      default: false,
+    });
+}
+
 const parser = yargs(hideBin(process.argv))
   .command({
     command: 'parse <file>',
     describe: 'parse a snax file without compiling it',
-    builder: (yargs) =>
-      yargs.positional('file', {
-        alias: 'file',
-        describe: 'file to parse',
-        type: 'string',
-      }),
-    handler: async (argv) => {
-      let file = argv.file as string;
-      const source = fs.readFileSync(file).toString();
+    builder,
+    handler: async (args) => {
+      const inPath = args.file;
+      const source = fs.readFileSync(inPath).toString();
       const ast = SNAXParser.parseStrOrThrow(source);
-      const outPath = fileWithExtension(file, '.ast.json');
+      const outPath = fileWithExtension(inPath, '.ast.json');
       fs.writeFileSync(outPath, JSON.stringify(ast));
       console.log(`wrote ${outPath}`);
     },
@@ -64,21 +76,16 @@ const parser = yargs(hideBin(process.argv))
   .command({
     command: 'compile <file>',
     describe: 'compile a snax file',
-    builder: (yargs) =>
-      yargs.positional('file', {
-        alias: 'file',
-        describe: 'file to compile',
-        type: 'string',
-      }),
-    handler: async (argv) => {
-      let file = argv.file as string;
-      console.log('Compiling file', file);
-      const maybeModule = await compileSnaxFile(file);
+    builder,
+    handler: async (args) => {
+      let inPath = args.file;
+      console.log('Compiling file', inPath);
+      const maybeModule = await compileSnaxFile(inPath);
       if (maybeModule.isOk()) {
         const result = maybeModule.value.toBinary({ write_debug_names: true });
-        fs.writeFileSync(fileWithExtension(file, '.wasm'), result.buffer);
+        fs.writeFileSync(fileWithExtension(inPath, '.wasm'), result.buffer);
         fs.writeFileSync(
-          fileWithExtension(file, '.wat'),
+          fileWithExtension(inPath, '.wat'),
           maybeModule.value.toText({})
         );
       } else {
@@ -90,27 +97,46 @@ const parser = yargs(hideBin(process.argv))
     command: 'run <file>',
     describe: 'compile and run a snax file',
     aliases: ['$0'],
-    builder: (yargs) =>
-      yargs.positional('file', {
-        alias: 'file',
-        describe: 'file to run',
-        type: 'string',
-      }),
-    handler: async (argv) => {
-      const maybeModule = await compileSnaxFile(argv.file as string);
-      if (maybeModule.isOk()) {
-        const result = maybeModule.value.toBinary({ write_debug_names: true });
-        const importObject = { wasi_unstable: wasi.wasiImport };
-        const wasm = await WebAssembly.compile(result.buffer);
-        const module = await WebAssembly.instantiate(wasm, importObject);
-        try {
-          wasi.start(module);
-        } catch (e) {
-          console.error(`error while running ${argv.file}`);
-          console.error(e);
+    builder,
+    handler: async (args) => {
+      const inPath = args.file;
+      const { ext } = path.parse(inPath);
+      const importObject = {
+        wasi_unstable: wasi.wasiImport,
+        wasi_snapshot_preview1: wasi.wasiImport,
+      };
+      let wasm: WebAssembly.Module;
+      if (ext === '.snx') {
+        const label = `compiling ${inPath}`;
+        if (args.verbose) console.time(label);
+        const maybeModule = await compileSnaxFile(inPath);
+        if (args.verbose) console.timeEnd(label);
+        if (maybeModule.isOk()) {
+          const result = maybeModule.value.toBinary({
+            write_debug_names: true,
+          });
+          wasm = await WebAssembly.compile(result.buffer);
+        } else {
+          console.error(maybeModule.error);
+          return;
         }
+      } else if (ext === '.wasm') {
+        wasm = await WebAssembly.compile(await readFile(inPath));
       } else {
-        console.error(maybeModule.error);
+        console.error(`Unrecognized extension ${ext}`);
+        return;
+      }
+
+      const module = await WebAssembly.instantiate(wasm, importObject);
+      try {
+        const label = `running ${inPath}`;
+        if (args.verbose) console.time(label);
+        wasi.start(module);
+        if (args.verbose) console.timeEnd(label);
+      } catch (e) {
+        console.error(`error while running ${args.file}`);
+        console.error(e);
+        return;
       }
     },
   });

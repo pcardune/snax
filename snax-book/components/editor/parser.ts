fc68b26b8c79e2f30762cc52fdafc428f4f4e66b
Paul Carduner <paul@carduner.net>
2023-10-21 12:03:16 -0700

    Update codemirror grammar and syntax highlighting styles

--- snax-book/components/editor/parser.ts
+++ snax-book/components/editor/parser.ts
@@ -18,6 +18,7 @@ let parserWithMetadata = parser.configure({
       '⚠': t.invalid,
       Identifier: t.local(t.variableName),
       NumberLiteral: t.number,
+      'NumberLiteral/identifier': t.typeName,
       Boolean: t.bool,
       String: t.string,
       Char: t.literal,
@@ -28,14 +29,16 @@ let parserWithMetadata = parser.configure({
       'ImportStatement/Identifier': t.local(t.variableName),
       'StructPropDecl/Identifier': t.propertyName,
       'StructLiteral/Identifier': t.typeName,
-      'StructPropValue/identifier': t.definition(t.propertyName),
-      'ParameterList/Identifier': [t.variableName, t.emphasis],
+      'StructPropValue/Identifier': t.definition(t.propertyName),
+      'ParameterList/identifier': [t.variableName, t.emphasis],
       'TypeExpr TypeExpr/Identifier': t.typeName,
       CompilerCall: t.special(t.variableName),
-      'FuncDeclBegin/Identifier': t.definition(t.variableName),
+      'FuncDeclBegin/Identifier': t.function(t.variableName),
+      'ModuleDecl/Identifier': t.definition(t.variableName),
       'VarDecl/VarName': t.definition(t.variableName),
       'while if else return': t.controlKeyword,
-      'global reg let func struct': t.definitionKeyword,
+      return: t.keyword,
+      'global reg let func struct module': t.definitionKeyword,
       pub: t.annotation,
       'CastExpr CastExpr/as': t.operatorKeyword,
       '( )': t.paren,
@@ -52,6 +55,14 @@ let parserWithMetadata = parser.configure({
     }),
   ],
 });
+
+export const exported = 123;
+export function foo(a: boolean) {
+  let b = 123;
+  if (true) {
+    console.log('whatever');
+  }
+}
 
 export const exampleLanguage = LRLanguage.define({
   parser: parserWithMetadata,
@@ -70,17 +81,22 @@ export const exampleCompletion = exampleLanguage.data.of({
     { label: 'global', type: 'keyword' },
     { label: 'while', type: 'keyword' },
     { label: 'return', type: 'keyword' },
+    { label: 'module', type: 'keyword' },
     { label: '$heap_start', type: 'function' },
   ]),
 });
 
 const myHighlightStyle = HighlightStyle.define([
-  { tag: t.variableName, color: '#0000ff' },
+  { tag: t.keyword, color: 'rgb(175,1,219)' },
+  { tag: t.variableName, color: 'rgb(6,113,194)' },
   { tag: t.propertyName, color: '#bbbb00' },
-  { tag: t.definitionKeyword, color: '#00aa00' },
-  { tag: t.controlKeyword, color: '#cc5500' },
-  { tag: t.function(t.variableName), color: '#ff0000' },
+  { tag: t.definitionKeyword, color: '#0000ff' },
+  { tag: t.controlKeyword, color: 'rgb(175,1,219)' },
+  { tag: t.definition(t.propertyName), color: '#bbbb00' },
+  { tag: t.function(t.variableName), color: 'rgb(123,97,43)' },
   { tag: t.invalid, color: 'red', 'text-decoration': 'underline' },
+  { tag: t.typeName, color: 'rgb(37, 127, 153)' },
+  { tag: t.lineComment, color: 'rgb(0,128,0)'},
 ]);
 
 export function example() {

e09c4bcd24162c5536ecc1eb500e8acfb661bbfc
Paul Carduner <paul@carduner.net>
2023-10-25 16:55:00 -0400

    Attempt to build vscode extension in github actions

--- vscode-extension/client/src/extension.ts
+++ vscode-extension/client/src/extension.ts
@@ -18,7 +18,7 @@ let client: LanguageClient;
 export function activate(context: ExtensionContext) {
   // The server is implemented in node
   const serverModule = context.asAbsolutePath(
-    path.join('server', 'server.cjs')
+    path.join('server', 'out', 'server-entry.js')
   );
 
   // If the extension is launched in debug mode then the debug server options are used
